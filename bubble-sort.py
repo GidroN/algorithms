1246ceb4c66b98130650cd5a6da2c03d6e98106e
Anton Illenberger <51234797+GidroN@users.noreply.github.com>
2021-12-16 20:48:01 +0400

    Update bubble-sort.py

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,11 +1,12 @@
-def bubble_sort(nums):
+def bubble_sort(nums):  
+    # Устанавливаем swapped в True, чтобы цикл запустился хотя бы один раз
     swapped = True
-
     while swapped:
         swapped = False
         for i in range(len(nums) - 1):
             if nums[i] > nums[i + 1]:
+                # Меняем элементы
                 nums[i], nums[i + 1] = nums[i + 1], nums[i]
-
+                # Устанавливаем swapped в True для следующей итерации
                 swapped = True
     return nums
